e667ffd9bc78be14e66e1c03495438e8bbfaafdc
LlucSF <lluc.semente@urv.cat>
2020-02-03 23:07:43 +0100

    - Code seems complete. Entering testing stage tomorrow.

--- R/raman2imzML.R
+++ R/raman2imzML.R
@@ -1,5 +1,5 @@
 #########################################################################
-#     Rqaman2imzML - Reinshaw's raman data in .txt format converter to 
+#     Raman2imzML - Reinshaw's raman data in .txt format converter to 
 #                    imzML using rMSI's imzML creator source code.
 #
 #     Copyright (C) 2020 Lluc Sementé Fernández
@@ -18,33 +18,67 @@
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ############################################################################
 
-txt_path<- "/home/lluc/Documents/PhDprogram/Data/Raman/20190206/BSi_AuNP_FP_633_5%_3s_static1200_map3.txt"
-
-raman_2_imzml <- function(txt_path, imzML_path)
+#' convert: converts .txt files from Renishaw's raman instruments and transforms it into .imzML files 
+#' 
+#' @param txt_path path to the .txt file.
+#' @param imzML_path path to the folder where the .imzML file is going to be stored.
+#'
+#' @return  TRUE if everything is alright.
+#' @export
+#'
+convert <- function(txt_path, imzML_path)
 {
+  #Get file name
+  file_name <- unlist(strsplit(txt_path, split = "/"))
+  file_name <- file_name[length(file_name)]
+  file_name <- unlist(strsplit(file_name,split = ".txt"))
+  
   #Raw data read and cleaning 
-  my_raw_data <- as.data.frame(utils::read.table(file = txt_path, header = FALSE))
-  my_clean_data <- raw_2_clean(my_raw_data)
-  rm(my_raw_data)
+  raw_data <- as.data.frame(utils::read.table(file = txt_path, header = FALSE))
+  clean_data <- raw_2_clean(raw_data)
+  rm(raw_data)
   
-  #UUID generating and .ibd file creation
-  my_uuid <- rMSI:::uuid_timebased()
-  write_ibd(imzML_path,my_clean_data, my_uuid)
+  #UUID generation, .ibd file creation and hash calculation 
+  uuid <- rMSI:::uuid_timebased()
+  hash <- write_ibd(imzML_path, file_name, clean_data, uuid)
+  clean_data$hash <- hash
+  clean_data$hash$uuid <- uuid
   
-  
+  #Genereting the offset matrix and the .imzML file
+  offset_matrix <- clean_2_offsetMatrix(clean_data)
+  return(CimzMLStore(paste(imzML_path, file_name, sep = ""), offset_matrix))
 }
 
+
+#' raw_2_clean
+#' 
+#' @param raw_data raw_data.
+#'
+#' @return  clean data.
+#' 
 raw_2_clean <- function(raw_data)
 {
   #Raman Shift Axis
   raman_shift <- union(raw_data[,3],raw_data[,3])
   band_length <- length(raman_shift)
   
-  #Motor coordenates
+  #Motor and relative coordenates
   x <- raw_data[seq(from = 1, to = nrow(raw_data)-(band_length-1), by = band_length), 1]
   y <- raw_data[seq(from = 1, to = nrow(raw_data)-(band_length-1), by = band_length), 2]
   number_pixels <- length(x)
-  motor_coords <- list(X = x, Y = y)
+  pixel_size <- abs(x[1]-x[2])
+  motor_coords <- list(x = x, y = y)
+  
+  rel_x <- x
+  for(i in 1:length(x)){
+    rel_x[i] <- which(rel_x[i] == unique(x))
+  }
+  
+  rel_y <- y
+  for(i in 1:length(y)){
+    rel_y[i] <- which(rel_y[i] == unique(y))
+  }
+  rel_coords <- list(x = rel_x, y = rel_y)
   
   #Intensity data filling
   intensity <- matrix(ncol = length(raman_shift), nrow = number_pixels)
@@ -58,23 +92,78 @@ raw_2_clean <- function(raw_data)
   #Clean data list creation
   clean_data <- list(ramanShift = raman_shift,
                      intensity = intensity,
-                     posMotors = motor_coords)
+                     posMotors = motor_coords,
+                     pos = rel_coords,
+                     pixelSize = pixel_size)
   
   return(clean_data)
 }
 
-clean_2_run <- function(clean_data)
+
+#' write_ibd
+#' 
+#' @param imzML_path raw_data.
+#' @param file_name raw_data.
+#' @param clean_data raw_data.
+#' @param uuid raw_data.
+#' 
+#' @return  clean data.
+#' 
+write_ibd <- function(imzML_path, file_name, clean_data, uuid)
 {
-  run_info <- list()
+  ibd_path <- paste(imzML_path,file_name,".ibd",sep = "")
+  intUUID <- strtoi(substring(uuid, seq(1,nchar(uuid),2), seq(2,nchar(uuid),2)), base = 16)
   
+  if (file.create(ibd_path))
+  {
+    ibd_conn <- file(ibd_path, "wb")
+    writeBin(intUUID, ibd_conn, size = 1, endian = "little") #write UUID
+    writeBin(clean_data$ramanShift, ibd_conn, size = 4, endian = "little", useBytes = T) #write raman shift axis
+    for(pixel in 1:nrow(clean_data$intensity))
+    {
+      writeBin(clean_data$intensity[pixel,], ibd_conn, size = 4, endian = "little", useBytes = T) #write each pixel
+    }
+    sha <- digest::digest(ibd_conn, algo = "sha1")
+    md5 <- digest::digest(ibd_conn, algo = "md5")
+    close(ibd_conn)
+  }
+  else(stop("File could not be created"))
+  
+  return(list(sha = sha, md5 = md5))
 }
 
-write_ibd <- function(imzML_path, clean_data, uuid)
+
+#' clean_2_offsetMatrix
+#' 
+#' @param clean_data path to the .txt file.
+#'
+#' @return  TRUE if everything is alright.
+#' 
+clean_2_offsetMatrix <- function(clean_data)
 {
+  # Create the offset matrix
+  offMat <- list()
+  offMat$UUID <- clean_data$hash$uuid
+  offMat$SHA <- clean_data$hash$sha
+  offMat$MD5 <-  clean_data$hash$md5
+  offMat$continuous_mode <- TRUE
+  offMat$compression_mz <- FALSE
+  offMat$compression_int <- FALSE
+  offMat$mz_dataType <- "float"
+  offMat$int_dataType <- "float"
+  offMat$pixel_size_um <- clean_data$pixelSize
   
+  offMat$run_data <- data.frame(x = clean_data$pos$x,
+                                y = clean_data$pos$y,
+                                mzLength = rep(length(clean_data$ramanShift), length(clean_data$pos$x)),
+                                mzOffset = rep(16, length(clean_data$pos$x)),
+                                intLength = rep(length(clean_data$ramanShift), length(clean_data$pos$x)),
+                                intOffset =  16+(length(clean_data$ramanShift)*4)+(4*length(clean_data$ramanShift)*(0:(length(clean_data$pos$x) - 1)))
+  )
+  
+  return(offMat)
   
 }
-
 
 
 
